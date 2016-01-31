260a24d8b933b04001095def0ad3fb2ab854ecf8
ishe_ua <in.shevkun@gmail.com>
2016-01-31 17:44:44 +0200

    clock.rb

---
+++ config/initializers/01_libs.rb
@@ -0,0 +1,11 @@
+# coding: utf-8
+# Подгружаем либы
+#
+
+require Rails.root.join('lib', 'app.rb')
+require Rails.root.join('lib', 'clock.rb')
+
+##
+
+# require Rails.root.join('lib/reader', 'reader.rb')
+# require Rails.root.join('lib/notificator', 'notificator.rb')
---
+++ lib/clock.rb
@@ -0,0 +1,41 @@
+# coding: utf-8
+# frozen_string_literal: true
+require 'clockwork'
+require 'active_job'
+
+# require './config/initializers/active_job'
+# require './config/initializers/gem_backburner'
+
+Dir['./app/jobs/**/*.rb'].each do |f|
+  require f
+end
+
+##
+# <b>"Часики"</b> проекта.
+#
+# Запускают задачки по рассписанию.
+#
+module Clockwork
+  ##
+  # Временная зона "часиков".
+  # Все время выставляется в ней.
+  #
+
+  TZ = 'Kyiv'.freeze
+
+  ## Каталог log-файла.
+  LOG_DIR = './log'.freeze
+
+  ## Назва log-файла.
+  LOG_FILE = 'clockwork.log'.freeze
+
+  configure do |config|
+    config[:tz] = TZ
+    config[:logger] = Logger.new("#{LOG_DIR}/#{LOG_FILE}") if
+      Dir.exist?(LOG_DIR)
+  end
+
+  ###
+  #
+  #
+end
