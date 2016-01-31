21351fc741bd86d94c99ce9a9d753194088581a0
ishe_ua <in.shevkun@gmail.com>
2016-01-31 10:06:52 +0200

    Fix rubocop issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
+# coding: utf-8
+# Базовый контроллер.
+#
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
+# coding: utf-8
+# Общий хелпер.
+#
 module ApplicationHelper
 end
