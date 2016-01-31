3f4d38ddb21535fa545c2659e78a1e5b300146d2
ishe_ua <in.shevkun@gmail.com>
2016-01-31 11:42:49 +0200

    Install annotate task

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,18 @@
 # coding: utf-8
+# == Schema Information
+#
+# Table name: accounts
+#
+#  id         :integer          not null, primary key
+#  email      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_accounts_on_email  (email) UNIQUE
+#
+
 ##
 # Зарегистрированный аккаунт.
 #
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,24 @@
 # coding: utf-8
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  account_id :integer
+#  jabber     :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_account_id  (account_id)
+#  index_users_on_jabber      (jabber) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_61ac11da2b  (account_id => accounts.id)
+#
+
 ##
 # Пользователь приложения, по сути его профиль.
 #
