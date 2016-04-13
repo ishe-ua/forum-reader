5d42ae1b32e2c63d2ce117ac038ee04dc6c71eef
ishe_ua <in.shevkun@gmail.com>
2016-04-13 17:00:40 +0300

    Dry

--- app/models/concerns/password.rb
+++ app/models/concerns/password.rb
@@ -25,16 +25,9 @@ module Password
   end
 
   class_methods do
-    ## Генерим случайный  и возвращаем как строку.
+    ## Генерим случайный пароль и возвращаем как строку.
     def gen_random_password
-      length = MIN_PASSWORD * 4
-
-      s1 = ('A'..'Z').to_a.shuffle
-      s2 = ('a'..'z').to_a.shuffle
-      s3 = (0..9)    .to_a.shuffle
-
-      array = (s1 + s2 + s3).shuffle
-      array[0..(length - 1)].join
+      Tokenizer.random_string()
     end
   end
 end
