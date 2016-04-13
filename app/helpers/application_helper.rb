269d08c1144cc72a359d1e6a773d105f10ff8f11
ishe_ua <in.shevkun@gmail.com>
2016-04-13 18:12:13 +0300

    application layout and images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,33 @@
 # coding: utf-8
-# Общий хелпер.
+# Общие хелперы приложения.
 #
 module ApplicationHelper
+  ## Title браузера.
+  def app_title
+    APP::NAME + ' | ' + t('app.description')
+  end
+
+  ##
+  # Годы жизни приложения. Два случая:
+  #
+  # * "2015" 1-й год жизни
+  # * "2015-2016" не 1-й
+  #
+
+  def app_years
+    year1 = APP::YEAR.to_i
+    year2 = Time.zone.now.year
+    year1 == year2 ? year1.to_s : "#{year1}-#{year2}"
+  end
+
+  ## Copyright-строка для footer-а.
+  def copyright
+    app_link = link_to APP::NAME.camelize, root_path
+    "&copy; «#{app_link}» #{app_years}. #{t('footer.copyright')}."
+  end
+
+  ## Показываем строку или APP::DASH если та пустая.
+  def dashed(str)
+    str.present? ? str : APP::DASH
+  end
 end
