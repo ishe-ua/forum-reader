e1e8eb89676453e25e8d020205de5f40454fe01e
ishe_ua <in.shevkun@gmail.com>
2016-04-14 11:02:34 +0300

    Link to wiki

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,9 @@
+# coding: utf-8
+# Хелперы для PagesController.
+#
 module PagesHelper
+  ## Ссылка на wiki-страницу с учетом текущей локали.
+  def xmpp_wiki_path
+    "http://#{I18n.locale}.wikipedia.org/wiki/XMPP"
+  end
 end
