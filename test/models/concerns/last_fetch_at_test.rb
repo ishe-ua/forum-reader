52760f6c5f3e2b7cfcb556ed07d78ef5866c5404
ishe_ua <in.shevkun@gmail.com>
2016-05-17 15:06:48 +0300

    Dry

--- test/models/concerns/last_fetch_at_test.rb
+++ test/models/concerns/last_fetch_at_test.rb
@@ -2,33 +2,26 @@ require 'test_helper'
 
 class LastFetchAtTest < ActiveSupport::TestCase
   setup do
-    @instance = build(:feed)
-  end
-
-  test 'can be empty' do
-    instance.last_fetch_at = nil
-    assert instance.valid?
+    @instance = feeds(:lbua)
   end
 
   test '#time_to_fetch? => true' do
     assert instance_class.time_to_fetch?('http://example.com/left-feed'),
            'feed not found'
 
-    feed = feeds(:lbua)
-    feed.update!(last_fetch_at: nil)
+    instance.update!(last_fetch_at: nil)
 
-    assert instance_class.time_to_fetch?(feed.url),
+    assert instance_class.time_to_fetch?(instance.url),
            'last_fetch_at is empty'
 
     travel_to(1.day.from_now) do
-      assert instance_class.time_to_fetch?(feed.url),
+      assert instance_class.time_to_fetch?(instance.url),
              'time to fetch'
     end
   end
 
   test '#time_to_fetch? => false' do
-    feed = feeds(:lbua)
-    assert_not instance_class.time_to_fetch?(feed.url),
+    assert_not instance_class.time_to_fetch?(instance.url),
                'time to fetch'
   end
 end
