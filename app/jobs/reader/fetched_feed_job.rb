932a937cc3174f71bd2583524ab3ea98dd3cc3a7
ishe_ua <in.shevkun@gmail.com>
2016-05-23 17:58:26 +0300

    FetchedFeedJob template

--- app/jobs/reader/fetched_feed_job.rb
+++ app/jobs/reader/fetched_feed_job.rb
@@ -3,6 +3,9 @@ module Reader
   class FetchedFeedJob < ActiveJob::Base
     queue_as :default
 
+    # See #find_or_create_feed_by
+    attr_reader :feed
+
     # Params:
     # - +url+ Url of Feed
     # - +response+ Response from Fetcher::process_single
@@ -12,8 +15,8 @@ module Reader
       Fetcher.raise_if_bad(resource_type)
       find_or_create_feed_by(url)
 
-      parse_and_save_updates(url, response)
-      send_forum_updates_to_users(url) if resource_type == Forum.to_s
+      parse_and_save_updates(response)
+      send_forum_updates_to_users if resource_type == Forum.to_s
     end
 
     protected
@@ -22,12 +25,35 @@ module Reader
       @feed ||= Feed.find_or_create_by(url: url)
     end
 
-    def parse_and_save_updates(url, response)
-      # TODO
+    def parse_and_save_updates(response)
+      response.entries.each do |entry|
+        feed_item = build_feed_item_from(entry)
+        feed_item.save if feed.last_fetch_at.blank? ||
+                          feed.last_fetch_at < feed_item.date
+      end
     end
 
-    def send_forum_updates_to_users(url)
-      # TODO
+    def send_forum_updates_to_users
+      Forum.where(url: feed.url).find_each do |forum|
+        # TODO: send jabber msg or email to user
+      end
+    end
+
+    private
+
+    def build_feed_item_from(entry)
+      feed_item = feed.build_feed_item(
+        url:   purify(entry.url),
+        date:  entry.data,
+        theme: purify(entry.title),
+        text:  purify(entry.text)
+      )
+
+      feed_item
+    end
+
+    def purify(str)
+      str.present? ? str.strip : ''
     end
   end
 end
