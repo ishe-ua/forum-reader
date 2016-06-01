e4903181d9ca049f4aeb4f4da2d19dec1694c5fa
ishe_ua <in.shevkun@gmail.com>
2016-06-01 10:17:30 +0300

    Dry BoteOut

--- lib/reader/bote_out.rb
+++ lib/reader/bote_out.rb
@@ -6,39 +6,29 @@ module Reader
   # Send Jabber messages to User.
   module BoteOut
     extend Blather::DSL
-    setup(Reader::BOTE_JID, Reader::BOTE_PASSWORD)
+    extend Utils::Queues
 
-    # Reconnect
+    setup(Reader::BOTE_JID, Reader::BOTE_PASSWORD)
     disconnected { client.connect }
 
+    # Queue with incoming messages (from Cmd).
     QUEUE_NAME = 'reader.bote_out'.to_sym
 
     class << self
       def run
         EM.run do
           client.run
-          EM.add_periodic_timer(1) { process_jobs_from_tube }
+          # rubocop:disable LineLength
+          EM.add_periodic_timer(1) { jobs_from(QUEUE_NAME) { |job| process_incoming(job) } }
         end
       end
 
       protected
 
-      # Process job from QUEUE_NAME.
-      def process_single(job)
-        body = Utils::Queues.args_from(job).first
-        to = Utils::Queues.args_from(job).second
+      def process_incoming(job)
+        body = args_from(job).first
+        to = args_from(job).second
         say(to, body)
-      end
-
-      private
-
-      def process_jobs_from_tube
-        @tube ||= Utils::Queues.get_tube(QUEUE_NAME)
-        while @tube.peek(:ready)
-          job = @tube.reserve
-          process_single(job)
-          job.delete
-        end
       end
     end
   end
--- lib/utils/queues.rb
+++ lib/utils/queues.rb
@@ -5,22 +5,33 @@ module Utils
   module Queues
     extend Backburner::Helpers
 
-    class << self
-      # Get access to Beanstalkd tube.
-      def get_tube(name)
-        conn = Backburner::Connection.new(Backburner.configuration.beanstalk_url)
-        tube = conn.tubes[expand_tube_name(name)]
-        tube
-      end
+    # Get access to Beanstalkd tube.
+    def get_tube(name)
+      conn = Backburner::Connection.new(Backburner.configuration.beanstalk_url)
+      tube = conn.tubes[expand_tube_name(name)]
+      tube
+    end
 
-      # Get job arguments from Beaneater::Job.
-      #
-      # Example:
-      # url = Helpers.args_from(job).first
+    # Get job arguments from Beaneater::Job.
+    #
+    # Example:
+    # url = Helpers.args_from(job).first
 
-      def args_from(job)
-        raise 'left class' unless job.is_a?(Beaneater::Job)
-        JSON.parse(job.body)['args'].first['arguments']
+    def args_from(job)
+      raise 'left class' unless job.is_a?(Beaneater::Job)
+      JSON.parse(job.body)['args'].first['arguments']
+    end
+
+    # Get batch of jobs from tube and process each.
+    #
+    # Cache #get_tube is present.
+
+    def jobs_from(tube_name)
+      @tube ||= get_tube(tube_name)
+      while @tube.peek(:ready)
+        job = @tube.reserve
+        yield(job)
+        job.delete
       end
     end
   end
