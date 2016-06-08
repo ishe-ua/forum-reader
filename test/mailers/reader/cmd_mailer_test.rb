f9f238fe7fcbebef3dc0f9289e5eaed8b9edbcd1
ishe_ua <in.shevkun@gmail.com>
2016-06-08 16:54:34 +0300

    forum_news email test

--- test/mailers/reader/cmd_mailer_test.rb
+++ test/mailers/reader/cmd_mailer_test.rb
@@ -13,6 +13,17 @@ module Reader
       assert_equal mail.to, [@account.email]
     end
 
+    test 'forum_news' do
+      forum = Forum.first
+      feed_item = FeedItem.first
+
+      target = Target::SUPPORTED_TARGETS.shuffle.sample.to_sym
+      mail = mailer.forum_news(forum, feed_item, target)
+
+      assert_equal mail.to, [forum.user.jabber] if target == :jabber
+      assert_equal mail.to, [forum.user.account.email] if target == :email
+    end
+
     test 'selection' do
       plus = [true, false].shuffle.sample
       mail = mailer.selection(@user, FeedItem.all, plus)
