4b9408fcbc3341864d93ae3abcd912afe187a176
ishe_ua <in.shevkun@gmail.com>
2016-06-08 16:44:09 +0300

    Selection mail test

--- test/mailers/reader/cmd_mailer_test.rb
+++ test/mailers/reader/cmd_mailer_test.rb
@@ -4,16 +4,21 @@ module Reader
   class CmdMailerTest < ActionMailer::TestCase
     setup do
       @mailer = CmdMailer
-      @account = accounts(:john)
+      @user = users(:john)
+      @account = @user.account
     end
 
     test 'help' do
-      mail = mailer.help(@account.user)
-      assert_equal [@account.email], mail.to
+      mail = mailer.help(@user)
+      assert_equal mail.to, [@account.email]
     end
 
-    test 'truth' do
-      skip
+    test 'selection' do
+      plus = [true, false].shuffle.sample
+      mail = mailer.selection(@user, FeedItem.all, plus)
+
+      assert_equal mail.to, [@user.jabber]
+      assert_equal mail.body.encoded.include?(FeedItem.first.text), plus
     end
   end
 end
