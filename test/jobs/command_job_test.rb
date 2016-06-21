48518315deee53e1e96eaf7aef9e2192d419a610
ishe_ua <in.shevkun@gmail.com>
2016-06-21 06:04:51 +0300

    CommandJob#find_user_from test

--- test/jobs/command_job_test.rb
+++ test/jobs/command_job_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class CommandJobTest < ActiveJob::TestCase
-  test 'the truth' do
-    skip
+  setup do
+    @instance = CommandJob.new
+  end
+
+  test 'find_user_from' do
+    user = instance.send(:find_user_from, 'ishe@example.com/ipad')
+    assert_equal user, users(:ishe)
   end
 end
