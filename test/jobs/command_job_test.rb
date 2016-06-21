c0213ac95d015d2d2eff54df2210f43883d747c1
ishe_ua <in.shevkun@gmail.com>
2016-06-21 06:13:59 +0300

    find_name_from test

--- test/jobs/command_job_test.rb
+++ test/jobs/command_job_test.rb
@@ -10,6 +10,12 @@ class CommandJobTest < ActiveJob::TestCase
     assert_equal user, users(:ishe)
   end
 
+  test 'find_name_from' do
+    assert_equal instance.send(:find_name_from, 'bbc'), 'bbc'
+    assert_equal instance.send(:find_name_from, 'bbc+'), 'bbc'
+    assert_equal instance.send(:find_name_from, ['', nil].shuffle.sample), ''
+  end
+
   test 'with_plus?' do
     assert instance.send(:with_plus?, 'bbc+')
     assert_not instance.send(:with_plus?, 'bbc')
