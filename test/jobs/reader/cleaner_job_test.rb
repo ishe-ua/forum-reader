3c3ca45e90aa2c8c727f7b795cb2ee14f9a412ad
ishe_ua <in.shevkun@gmail.com>
2016-06-22 15:46:28 +0300

    Dry

--- test/jobs/reader/cleaner_job_test.rb
+++ test/jobs/reader/cleaner_job_test.rb
@@ -9,26 +9,30 @@ module Reader
     end
 
     test 'find_feed_items_for_clean => found' do
-      prepare_data(count: CleanerJob::MIN_STORE_ITEMS + 1)
-      rel = job.send(:find_feed_items_for_clean, @feed)
-      assert rel.count > 0
+      count = CleanerJob::MIN_STORE_ITEMS + 1
+      assert_with(count)
     end
 
     test 'find_feed_items_for_clean => not found' do
-      prepare_data(count: CleanerJob::MIN_STORE_ITEMS - 1)
-      rel = job.send(:find_feed_items_for_clean, @feed)
-      assert rel.count < CleanerJob::MIN_STORE_ITEMS
+      count = CleanerJob::MIN_STORE_ITEMS - 1
+      assert_with(count)
     end
 
     private
 
-    def prepare_data(opts = {})
+    def assert_with(count)
+      prepare_data(count)
+      sel = job.send(:find_feed_items_for_clean, @feed)
+      assert_equal sel.count, count
+    end
+
+    def prepare_data(count)
       time = CleanerJob::MIN_STORE_TIME + 1.day
       travel_to(time.ago) do
-        opts[:count].times do |i|
+        count.times do |i|
           fi = build(:feed_item, url: "http://example.com/#{i}")
           fi.feed_id = @feed.id
-          fi.save
+          fi.save!(validate: false)
         end
       end
     end
