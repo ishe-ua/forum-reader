9df0a61dbeab3097f12e70429e2d6c060f7465e4
ishe_ua <in.shevkun@gmail.com>
2016-06-23 09:45:09 +0300

    FetchForumsJob test

--- test/jobs/reader/fetcher/fetch_forums_job_test.rb
+++ test/jobs/reader/fetcher/fetch_forums_job_test.rb
@@ -3,8 +3,12 @@ require 'test_helper'
 module Reader
   module Fetcher
     class FetchForumsJobTest < ActiveJob::TestCase
-      test 'the truth' do
-        skip
+      setup do
+        @job = FetchForumsJob
+      end
+
+      test 'success' do
+        assert_enqueued_jobs(Forum.count) { job.perform_now }
       end
     end
   end
