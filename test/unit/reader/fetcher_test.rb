d1ab53c98126822da1dd73b52e8aa1d898143178
ishe_ua <in.shevkun@gmail.com>
2016-06-27 16:02:54 +0300

    Fetcher tests

--- test/unit/reader/fetcher_test.rb
+++ test/unit/reader/fetcher_test.rb
@@ -5,5 +5,13 @@ module Reader
     setup do
       @modul = Fetcher
     end
+
+    test 'run' do
+      modul.respond_to?(:run)
+    end
+
+    test 'process_incoming' do
+      modul.respond_to?(:process_incoming)
+    end
   end
 end
