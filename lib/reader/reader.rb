6a0caf19adbee4ca5b8a4cb3fa30ee57bd98f289
ishe_ua <in.shevkun@gmail.com>
2016-07-05 09:11:06 +0300

    Doc for Sender module

--- lib/reader/reader.rb
+++ lib/reader/reader.rb
@@ -9,8 +9,9 @@ module Reader
 
   if defined?(Rails)
     require_relative 'cmd.rb' # should be first
-    require_relative 'fetcher.rb'
     require_relative 'bote_in.rb'
     require_relative 'bote_out.rb'
+    require_relative 'fetcher.rb'
+    require_relative 'sender.rb'
   end
 end
---
+++ lib/reader/sender.rb
@@ -0,0 +1,6 @@
+module Reader
+  # Send Forum -s and Letter -s to Target -s
+  #
+  module Sender
+  end
+end
