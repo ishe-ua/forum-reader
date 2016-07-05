489965d5b53bac2a6ad0c8d6c3acc606c7c842f2
ishe_ua <in.shevkun@gmail.com>
2016-07-05 10:22:35 +0300

    Review require Utils

--- config/initializers/01_libs.rb
+++ config/initializers/01_libs.rb
@@ -4,13 +4,7 @@
 require Rails.root.join('lib', 'app.rb')
 require Rails.root.join('lib', 'clock.rb')
 
-##
-
-require Rails.root.join('lib/utils', 'convert.rb')
-require Rails.root.join('lib/utils', 'queues.rb')
-require Rails.root.join('lib/utils', 'tokenizer.rb')
-
-##
+require Rails.root.join('lib/utils', 'utils.rb')
 
 require Rails.root.join('lib/reader', 'reader.rb')
 require Rails.root.join('lib/mailer', 'mailer.rb')
