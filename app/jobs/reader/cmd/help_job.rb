d679e852a143eccfa9d4d6a0ee66da62ab40ba40
ishe_ua <in.shevkun@gmail.com>
2016-07-05 08:53:29 +0300

    Fix

--- app/jobs/reader/cmd/help_job.rb
+++ app/jobs/reader/cmd/help_job.rb
@@ -9,8 +9,8 @@ module Reader
       def perform(body, from)
         return unless body =~ REGEXP
         if (user = find_user_from(from))
-          body = ReplyMailer.help(user).body.encoded
-          ReplyJob.perform_later(body, from) if body.present?
+          text = ReplyMailer.help(user).body.encoded
+          ReplyJob.perform_later(text, from) if text.present?
         end
       end
     end
