6e2d23242811fa3dae545c9ebc40453ab2ba9c5e
ishe_ua <in.shevkun@gmail.com>
2016-07-29 11:39:54 +0300

    Test

--- config/initializers/lib_piper.rb
+++ config/initializers/lib_piper.rb
@@ -5,18 +5,20 @@
 #
 # HACK!!
 
-if Rails.env.production?
-  gem_home =
-    "/home/deployer/#{APP::HOST}/shared/bundle/ruby/#{RUBY_VERSION}/gems"
-  gem_home_dirs = Dir.entries(gem_home)
+# if Rails.env.production?
+#   gem_home =
+#     "/home/deployer/#{APP::HOST}/shared/bundle/ruby/#{RUBY_VERSION}/gems"
+#   gem_home_dirs = Dir.entries(gem_home)
 
-  Mailer::Piper::REQUIRED_LIBS.each do |lib|
-    b = false
+#   Mailer::Piper::REQUIRED_LIBS.each do |lib|
+#     b = false
 
-    gem_home_dirs.each do |dirname|
-      b = dirname.start_with?(lib.remove('_') + '-') unless b
-    end
+#     gem_home_dirs.each do |dirname|
+#       b = dirname.start_with?(lib.remove('_') + '-') unless b
+#     end
 
-    raise "gem #{lib} not installed" unless b
-  end
-end
+#     raise "gem #{lib} not installed" unless b
+#   end
+# end
+
+# TODO: unncomment
