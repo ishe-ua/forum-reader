3744e9aecca8bda039949c8f22a0646b0037e95a
ishe_ua <in.shevkun@gmail.com>
2016-08-04 21:08:30 +0300

    Fix

--- lib/utils/tokenizer.rb
+++ lib/utils/tokenizer.rb
@@ -5,12 +5,6 @@ module Utils
 
     def self.gen_random_string(size = DEFAULT_SIZE)
       Array.new(size) { [*'0'..'9', *'a'..'z'].sample }.join
-
-      # chars = ('a'..'z').to_a.shuffle
-      # nums = (0..9).to_a.shuffle
-
-      # ar = (chars + nums).shuffle
-      # 1.upto(size).map { ar.sample }.join
     end
   end
 end
