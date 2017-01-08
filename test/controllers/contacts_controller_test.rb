5c2c4970adc3e55abc02541952b919fb22ebcf9b
ishe_ua <in.shevkun@gmail.com>
2017-01-08 23:35:57 +0200

    Update ContactsControllerTest

--- test/controllers/contacts_controller_test.rb
+++ test/controllers/contacts_controller_test.rb
@@ -1,38 +1,39 @@
 # coding: utf-8
 require 'test_helper'
 
-class ContactsControllerTest < ActionController::TestCase
+class ContactsControllerTest < ActionDispatch::IntegrationTest
   test 'should get new' do
-    get :new
+    get new_contact_path
     assert_response :success
   end
 
   test 'should create contact and send 2 emails' do
     assert_difference('Contact.count') do
-      assert_enqueued_jobs 2 do
-        post :create, contact:
-                        {
-                          email: 'user@example.com',
-                          theme: 'aa',
-                          text:  'bb'
-                        }
+      assert_enqueued_emails 2 do
+        post contacts_path, params: { contact:
+                                        {
+                                          email: 'user@example.com',
+                                          theme: 'aa',
+                                          text:  'bb'
+                                        } }
       end
 
-      assert_response :redirect
       assert_redirected_to info_path
     end
   end
 
   test 'should not create contact if заполнены не все поля, письма не шлются' do
     assert_no_difference('Contact.count') do
-      assert_no_enqueued_jobs do # письма не шлются через .deliver_later
-        assert_no_emails do      # письма не шлются через .deliver_later
-          post :create, contact:
-                          {
-                            email: 'user@example.com',
-                            theme: '',
-                            text:  ''
-                          }
+      assert_no_enqueued_emails do  # письма не шлются через .deliver_later
+        assert_no_emails do         # письма не шлются через .deliver_later
+          post contacts_path, params: {
+            contact:
+              {
+                email: 'user@example.com',
+                theme: '',
+                text:  ''
+              }
+          }
 
           assert_response :success
         end
