753858e34323afea9025601699b8d9bf2f2ba460
ishe_ua <in.shevkun@gmail.com>
2017-01-08 21:54:54 +0200

    Upgrade initializers

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,5 +1,5 @@
-ActionMailer::DeliveryJob.queue_name = 'default'
 am = ActionMailer::Base
+am.deliver_later_queue_name = 'default' # TODO: check
 
 am.default_options = { from: "#{APP::NAME} <#{APP::NOREPLY_EMAIL}>" }
 am.default_url_options = { host: 'localhost', port: 3000 }
