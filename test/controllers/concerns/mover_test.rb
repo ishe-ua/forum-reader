619dcb0d0f842c613c25bbecdf817b77b33cb85c
ishe_ua <in.shevkun@gmail.com>
2017-01-09 12:41:46 +0200

    Update MoverTets

--- test/controllers/concerns/mover_test.rb
+++ test/controllers/concerns/mover_test.rb
@@ -1,30 +1,31 @@
-# coding: utf-8
 require 'test_helper'
 
-class MoverTest < ActionController::TestCase
-  tests [ForumsController, LettersController].shuffle.sample
-
+class MoverTest < ActionDispatch::IntegrationTest
   setup do
     account = accounts(:ishe)
-    resources_name = @controller.send(:resources_name)
+    @resources_name = ['forums', 'letters'].sample
 
-    @resource1 = account.user.send(resources_name).first
-    @resource2 = account.user.send(resources_name).second
+    @resource1 = account.user.send(@resources_name).first
+    @resource2 = account.user.send(@resources_name).second
 
     sign_in(account)
   end
 
   test '#move_up' do
+    url = "move_up_#{@resources_name.singularize}_path"
+
     assert_resource1_less_resource2('1 higher')
-    patch :move_up, id: @resource2, format: :js
+    patch send(url, id: @resource2, format: :js)
 
     assert_response :success
     assert_resource2_less_resource1('2 higher')
   end
 
   test '#move_down' do
+    url = "move_down_#{@resources_name.singularize}_path"
+
     assert_resource1_less_resource2('1 higher')
-    patch :move_down, id: @resource1, format: :js
+    patch send(url, id: @resource1, format: :js)
 
     assert_response :success
     assert_resource2_less_resource1('2 higher')
