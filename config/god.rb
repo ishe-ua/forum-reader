590cedc81a7d0daab0314721441766e988e37741
ishe_ua <in.shevkun@gmail.com>
2017-01-13 16:14:15 +0200

    God config

---
+++ config/god.rb
@@ -0,0 +1,24 @@
+require_relative '../lib/app.rb'
+RAILS_ROOT = File.dirname(File.dirname(__FILE__))
+
+
+God.watch do |w|
+  w.name = APP::NAME + '-web'
+  w.dir = RAILS_ROOT
+
+  pid_file = File.join(RAILS_ROOT, 'tmp/puma.pid')
+
+  w.pid_file = pid_file
+  w.behavior(:clean_pid_file)
+
+  w.start = 'bundle exec puma'
+  w.stop = "kill -s QUIT $(cat #{pid_file})"
+  w.restart = "kill -s HUP $(cat #{pid_file})"
+
+  w.interval = 60.seconds
+  w.start_grace = 20.seconds
+  w.restart_grace = 20.seconds
+
+
+  w.env = { 'RAILS_ENV' => 'production' }
+end
