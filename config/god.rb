fb68e19c0caa458b061beaa0c8e848d63ab7fde2
ishe_ua <in.shevkun@gmail.com>
2017-01-13 16:32:22 +0200

    Test

--- config/god.rb
+++ config/god.rb
@@ -6,19 +6,18 @@ God.watch do |w|
   w.name = APP::NAME + '-web'
   w.dir = RAILS_ROOT
 
-  pid_file = File.join(RAILS_ROOT, 'tmp/puma.pid')
+  # pid_file = File.join(RAILS_ROOT, 'tmp/puma.pid')
 
-  w.pid_file = pid_file
-  w.behavior(:clean_pid_file)
+  # w.pid_file = pid_file
+  # w.behavior(:clean_pid_file)
 
   w.start = 'bundle exec puma'
-  w.stop = "kill -s QUIT $(cat #{pid_file})"
-  w.restart = "kill -s HUP $(cat #{pid_file})"
+  # w.stop = "kill -s QUIT $(cat #{pid_file})"
+  # w.restart = "kill -s HUP $(cat #{pid_file})"
 
-  w.interval = 60.seconds
+  w.interval = 10.seconds
   w.start_grace = 20.seconds
   w.restart_grace = 20.seconds
-
 
   w.env = { 'RAILS_ENV' => 'production' }
 end
