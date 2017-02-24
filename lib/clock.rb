ff418bc6ad3672688c42d5adf23b5ea722f5bb36
ishe_ua <in.shevkun@gmail.com>
2017-02-24 14:07:32 +0200

    Clock in Rails mode

--- lib/clock.rb
+++ lib/clock.rb
@@ -1,33 +1,22 @@
 require 'clockwork'
-require_relative 'app'
-require_relative '../config/initializers/active_job'
+
+require './config/boot'
+require './config/environment'
 
 # App clock, gem 'clockwork'.
 module Clockwork
-  # Always is UTC (+server+ setting).
-  TZ = 'UTC'.freeze
-
-  LOG_DIR = './log'.freeze
-  LOG_FILE = 'clock.log'.freeze
-
   # Time for system tasks.
-  SYS_TIME = '4:05'.freeze
-
-  configure do |config|
-    config[:tz] = TZ
-    config[:logger] = Logger.new("#{LOG_DIR}/#{LOG_FILE}") if Dir.exist? LOG_DIR
-  end
+  SYS_TIME = '4:00'.freeze
 
   # rubocop:disable LineLength
-  # rubocop:disable Rails/TimeZone
 
-  every(5.minutes, 'reader.fetch.forums') { Reader::Fetcher::FetchForumsJob.perform_later }
-  every(15.minutes, 'reader.fetch.letters') { Reader::Fetcher::FetchLettersJob.perform_later }
+  # every(5.minutes, 'reader.fetch.forums') { Reader::Fetcher::FetchForumsJob.perform_later }
+  # every(15.minutes, 'reader.fetch.letters') { Reader::Fetcher::FetchLettersJob.perform_later }
 
-  every(1.minute, 'reader.send.letters', if: ->(time) { APP::SUPPORTED_MINUTES.include?(time.min) }) do
-    Reader::Sender::SendLettersJob.perform_later(Time.now)
-  end
+  # every(1.minute, 'reader.send.letters', if: ->(time) { APP::SUPPORTED_MINUTES.include?(time.min) }) do
+  #   Reader::Sender::SendLettersJob.perform_later(Time.zone.now.to_i)
+  # end
 
-  every(1.day, 'reader.clean.feed_items', at: SYS_TIME) { Reader::CleanerJob.perform_later }
-  every(1.day, 'mailer.clean.messages', at: SYS_TIME) { Mailer::CleanerJob.perform_later }
+  # every(1.day, 'reader.clean.feed_items', at: SYS_TIME) { Reader::CleanerJob.perform_later }
+  # every(1.day, 'mailer.clean.messages', at: SYS_TIME) { Mailer::CleanerJob.perform_later }
 end
