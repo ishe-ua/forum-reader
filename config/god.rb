7807a84f83c1dda12496f879f2d4ae867e3545d9
ishe_ua <in.shevkun@gmail.com>
2017-02-24 16:57:01 +0200

    Review god.rb

--- config/god.rb
+++ config/god.rb
@@ -1,13 +1,24 @@
-RAILS_ROOT = File.expand_path('..', __dir__)
 require_relative '../lib/app.rb'
+
+RAILS_ROOT = File.expand_path('..', __dir__)
+RAILS_ENV = { 'RAILS_ENV' => 'production' }.freeze
 
 God.watch do |w|
   w.name  = APP::NAME + '-web'
   w.start = 'bundle exec puma'
 
   w.keepalive
-  w.group = 'web'
 
   w.dir = RAILS_ROOT
-  w.env = { 'RAILS_ENV' => 'production' }
+  w.env = RAILS_ENV
+end
+
+God.watch do |w|
+  w.name  = APP::NAME + '-clock'
+  w.start = 'bundle exec clockwork lib/clock.rb'
+
+  w.keepalive
+
+  w.dir = RAILS_ROOT
+  w.env = RAILS_ENV
 end
