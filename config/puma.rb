ff94bf2cf09bc5c9618bed442aee7013223c51b0
ishe_ua <in.shevkun@gmail.com>
2017-03-02 09:30:00 +0200

    Puma in single mode

--- config/puma.rb
+++ config/puma.rb
@@ -6,12 +6,12 @@ threads threads_count, threads_count
 bind "unix:/tmp/#{APP::NAME.tr('-', '_')}.sock"
 environment ENV.fetch('RAILS_ENV') { 'development' }
 
-workers ENV.fetch('WEB_CONCURRENCY') { 2 }
-preload_app!
+# workers ENV.fetch('WEB_CONCURRENCY') { 1 }
+# preload_app!
 
-on_worker_boot do
-  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
-end
+# on_worker_boot do
+#   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+# end
 
 # Allow puma to be restarted by `rails restart` command.
-plugin :tmp_restart
+# plugin :tmp_restart
