555518ef2a2fc25e680b5fe0c355b8d26c788109
ishe_ua <in.shevkun@gmail.com>
2017-04-04 17:03:32 +0300

    FetchFeed review

--- app/jobs/reader/fetch_feed_job.rb
+++ app/jobs/reader/fetch_feed_job.rb
@@ -1,17 +1,44 @@
 module Reader
-  # Fetch Feed from remote Url.
-  #
-  # Incoming job for Fetcher.
+  # Fetch and parse Feed from remote Url.
   class FetchFeedJob < ApplicationJob
     queue_as :default
 
-    # +Deprecated+ method (should performs in Fetcher).
-    #
-    # Params:
-    # - +url+ Url of Feed
+    def perform(url)
+      feed = find_or_create_feed_by(url)
+      news = find_news(feed)
+      SendForumsJob.perform_later(url) if news&.any?
+    end
 
-    def perform(_url)
-      raise 'Deprecated'
+    protected
+
+    def find_or_create_feed_by(url)
+      Feed.find_or_create_by!(url: url)
+    end
+
+    # <tt>WARN:</tt> core method!!
+    def find_news(feed)
+      Feedjira::Feed.fetch_and_parse(feed.url).entries.select do |entry|
+        feed_item = build_feed_item_from(entry, feed)
+        feed_item.save if feed.last_fetch_at.nil? ||
+                          feed.last_fetch_at < feed_item.date
+      end
+    rescue
+      nil
+    end
+
+    private
+
+    def build_feed_item_from(entry, feed)
+      feed.feed_items.new(
+        url:   purify(entry.url),
+        date:  entry.published,
+        theme: purify(entry.title),
+        text:  purify(entry.summary)
+      )
+    end
+
+    def purify(str)
+      str ? str.strip : ''
     end
   end
 end
--- test/jobs/reader/fetch_feed_job_test.rb
+++ test/jobs/reader/fetch_feed_job_test.rb
@@ -3,11 +3,39 @@ require 'test_helper'
 module Reader
   class FetchFeedJobTest < ActiveJob::TestCase
     setup do
-      @job = FetchFeedJob
+      @job = FetchFeedJob.new
     end
 
-    test 'exception if perform' do
-      assert_raise { job.perform_now(nil, nil) }
+    test 'perform' do
+      job.class.perform_now('http://example.com/feed')
+    end
+
+    test 'find_or_create_feed_by => finded' do
+      assert_no_difference 'Feed.count' do
+        url = feeds(:reddit_ruby).url
+        job.send(:find_or_create_feed_by, url)
+      end
+    end
+
+    test 'find_or_create_feed_by => created' do
+      assert_difference 'Feed.count' do
+        url = 'http://example.com/feed'
+        job.send(:find_or_create_feed_by, url)
+      end
+    end
+
+    test 'find_news => success' do
+      fixture_name = %i(reddit_ruby opennet lbua atytarenko).sample
+      VCR.use_cassette(fixture_name.to_s) do
+        feed = feeds(fixture_name)
+        feed.update!(last_fetch_at: 10.years.ago)
+
+        assert job.send(:find_news, feed)
+      end
+    end
+
+    test 'find_news => fail' do
+      assert_nil job.send(:find_news, nil)
     end
   end
 end
