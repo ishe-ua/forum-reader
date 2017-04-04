1e36e501124f17648c7d5ec4f3b732557c54f167
ishe_ua <in.shevkun@gmail.com>
2017-04-04 20:04:16 +0300

    Remove default mailer's queue name

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -14,7 +14,6 @@ end
 #
 
 am = ActionMailer::Base
-am.deliver_later_queue_name = 'mailer'
 
 am.default_options = { from: "#{APP::NAME} <#{APP::NOREPLY_EMAIL}>" }
 am.default_url_options = { host: 'localhost', port: 3000 } if
