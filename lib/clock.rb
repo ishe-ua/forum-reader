721322ea71a807e92151394b698218ae252f5169
ishe_ua <in.shevkun@gmail.com>
2017-04-17 17:28:25 +0300

    Remove CleanerJob

--- lib/clock.rb
+++ lib/clock.rb
@@ -27,7 +27,4 @@ module Clockwork
   every(1.minute, 'reader: send letters', if: ->(time) { APP::SUPPORTED_MINUTES.include?(time.min) }) do
     Reader::SendLettersJob.perform_later(Time.now.utc.to_i)
   end
-
-  every(1.day, 'reader: clean feed_items', at: SYS_TIME) { Reader::CleanerJob.perform_later }
-  # every(1.day, 'mailer.clean.messages', at: SYS_TIME) { Mailer::CleanerJob.perform_later }
 end
