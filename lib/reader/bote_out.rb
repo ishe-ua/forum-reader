7ed2a8face25ba7c6bd1b4314662e6f82774bbf0
ishe_ua <in.shevkun@gmail.com>
2017-04-18 14:25:47 +0300

    Jabber for bote_out

--- lib/reader/bote_out.rb
+++ lib/reader/bote_out.rb
@@ -1,4 +1,5 @@
 require 'bundler/setup'
+require 'blather/client/dsl'
 
 require_relative '../../config/initializers/active_job'
 require_relative 'reader.rb'
@@ -6,13 +7,16 @@ require_relative 'reader.rb'
 module Reader
   # Send Jabber messages to User.
   module BoteOut
+    extend Blather::DSL
+
+    setup(Reader::BOTE_JID, APP::BOTES_PASSWORD)
+    disconnected { client.connect }
+
     # Queue with incoming messages (from Cmd).
     QUEUE_NAME = 'reader_bote_out'.freeze
 
-    class << self
-      def run
-        sleep
-      end
+    def self.run
+      EM.run { client.run }
     end
   end
 end
