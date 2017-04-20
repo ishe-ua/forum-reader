d606fe4942e9d6b9517b236d8f5785237eab47fc
ishe_ua <in.shevkun@gmail.com>
2017-04-20 13:33:55 +0300

    Change sidekiq pool size

--- config/initializers/gem_sidekiq.rb
+++ config/initializers/gem_sidekiq.rb
@@ -4,9 +4,9 @@ require 'sidekiq'
 redis_conn = proc { $redis }
 
 Sidekiq.configure_client do |config|
-  config.redis = ConnectionPool.new(size: 1, &redis_conn)
+  config.redis = ConnectionPool.new(size: 25, &redis_conn)
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = ConnectionPool.new(size: 1, &redis_conn)
+  config.redis = ConnectionPool.new(size: 25, &redis_conn)
 end
