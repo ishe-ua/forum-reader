e6daa92de2450c9ae707807076bd4f606d37a784
ishe_ua <in.shevkun@gmail.com>
2017-05-10 02:10:10 +0300

    Remove AdminUser creation from seeds.rb

--- db/seeds.rb
+++
@@ -1,5 +0,0 @@
-unless AdminUser.any?
-  AdminUser.create!(email: APP::ADMIN_EMAIL,
-                    password: APP::DEFAULT_PASSWORD,
-                    password_confirmation: APP::DEFAULT_PASSWORD)
-end
